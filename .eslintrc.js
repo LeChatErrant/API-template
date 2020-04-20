8ea362bf01a2d4f44c0a39de003c2dea51c20c0a
LeChatErrant <guillaume.hector@epitech.eu>
2020-04-20 16:35:49 +0200

    fix(linter): missing eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser:  '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+  },
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-param-reassign': ['error', { 'props': false }],
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+  },
+};
