6516985caac182ae4883c846ad1927bd42505888
LeChatErrant <guillaume.hector@epitech.eu>
2020-04-26 20:17:23 +0200

    feat: validators and validateMiddleware

--- src/components/user/userTypes.ts
+++ src/components/user/userTypes.ts
@@ -1,16 +1,36 @@
-export interface UserCreation {
-  email: string;
-  name?: string;
-  password: string;
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UserSignup {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @IsOptional()
+  name!: string;
+
+  @IsString()
+  password!: string;
 }
 
-export interface UserModifiableFields {
-  email?: string;
-  name?: string;
-  password?: string;
+export class UserSignin {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  password!: string;
 }
 
-export interface UserCredentials {
-  email: string;
-  password: string;
+export class UserUpdate {
+  @IsEmail()
+  @IsOptional()
+  email!: string;
+
+  @IsString()
+  @IsOptional()
+  name!: string;
+
+  @IsString()
+  @IsOptional()
+  password!: string;
 }
+
---
+++ src/middlewares/validationMiddleware.ts
@@ -0,0 +1,19 @@
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { RequestHandler } from 'express';
+import httpStatus from 'http-status-codes';
+import createError from 'http-errors';
+
+function validationMiddleware<T>(type: any): RequestHandler {
+  return async (req, res, next) => {
+    const errors = await validate(plainToClass(type, req.body));
+    if (errors.length !== 0) {
+      const message = errors.join('');
+      next(createError(httpStatus.BAD_REQUEST, message));
+    } else {
+      next();
+    }
+  };
+}
+
+export default validationMiddleware;
