59b67370bc45335145bee01815e5676a1b93b251
LeChatErrant <guillaume.hector@epitech.eu>
2020-04-28 13:09:36 +0200

    feat: database connection at app start

--- src/appDatabase.ts
+++ src/appDatabase.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
+import logger from './appLogger';
+import { config } from './appConfig';
+
 const client = new PrismaClient();
+
+client
+  .connect()
+  .then(() => {
+    logger.info(`Connected to database at url ${config.db_url}`);
+  })
+  .catch(() => {
+    logger.info(`Can't connect to database as url ${config.db_url}`);
+  });
 
 export default client;
