40b843d9a552c4a928f2a94985e59812857fd118
LeChatErrant <guillaume.hector@epitech.eu>
2020-05-04 22:33:05 +0200

    fix(lint): disabled no use before define for variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'no-param-reassign': ['error', { 'props': false }],
     'import/prefer-default-export': 'off',
     'no-underscore-dangle': 'off',
-    'no-use-before-define': 'off',
+    'no-use-before-define': ['error', 'nofunc'],
     'no-restricted-syntax': [
       'error',
       {
