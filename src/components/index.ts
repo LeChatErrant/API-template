3798a6e51cb2e163917def88dc233f15667b4996
LeChatErrant <guillaume.hector@epitech.eu>
2020-09-22 19:08:03 +0200

    feat: simple ping (GET /ping)

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import users from './user/userRoutes';
 
 const router = express.Router();
 
+router.get('/ping', (req, res) => res.send('pong'));
 router.use('/users', users);
 
 export default router;
