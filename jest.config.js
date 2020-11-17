05f9d71eeab725e2b1b28cbbd87721bae4737283
LeChatErrant <guillaume.hector@epitech.eu>
2020-11-17 23:09:36 +0100

    feat: rootDir configured, avoiding building artifacts

--- jest.config.js
+++ jest.config.js
@@ -54,7 +54,7 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: './src/utils/waitApp.ts',
+  globalSetup: '<rootDir>/utils/waitApp.ts',
 
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
@@ -114,7 +114,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
+  rootDir: "./src",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
