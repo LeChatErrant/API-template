ddf682fb9e9bd94a55e3382a2b7c71c28fb5a18c
LeChatErrant <guillaume.hector@epitech.eu>
2020-12-16 17:33:40 +0100

    misc: removed useless comments

--- src/appLogger.ts
+++ src/appLogger.ts
@@ -14,19 +14,11 @@ const logger = winston.createLogger({
     customFormat,
   ),
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `all.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/all.log' }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (config.mode !== MODES.PROD) {
   logger.add(new winston.transports.Console());
 }
