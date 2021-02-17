2cf0d788b49e384e0c76f53155f6afff7e89a1da
LeChatErrant <guillaume.hector@epitech.eu>
2021-02-17 03:18:07 +0100

    misc: routers are now responsible for their whole path (enabling nested instances)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import users from './user/userRoutes';
+import posts from './post/postRoutes';
 
 const router = express.Router();
 
 router.get('/ping', (req, res) => res.send('pong'));
-router.use('/users', users);
+
+router.use(users);
+router.use(posts);
 
 export default router;
--- src/components/user/userRoutes.ts
+++ src/components/user/userRoutes.ts
@@ -11,17 +11,17 @@ import { UserSignupDto, UserSigninDto, UserUpdateDto } from './userTypes';
 
 const router = express.Router();
 
-router.get('/', adminMiddleware, handler(async (req, res) => {
+router.get('/users', adminMiddleware, handler(async (req, res) => {
   const users = await controllers.listUsers();
   res.send(users);
 }));
 
-router.post('/signup', validate(UserSignupDto), handler(async (req, res) => {
+router.post('/users/signup', validate(UserSignupDto), handler(async (req, res) => {
   const user = await controllers.signup(req.body);
   res.status(httpStatus.CREATED).send(user);
 }));
 
-router.post('/signin', validate(UserSigninDto), handler(async (req, res) => {
+router.post('/users/signin', validate(UserSigninDto), handler(async (req, res) => {
   const user = await controllers.signin(req.body);
   req.session.user = {
     id: user.id,
@@ -30,17 +30,17 @@ router.post('/signin', validate(UserSigninDto), handler(async (req, res) => {
   res.send(user);
 }));
 
-router.get('/:userId', userMiddleware, handler(async (req, res) => {
+router.get('/users/:userId', userMiddleware, handler(async (req, res) => {
   const user = await controllers.getUser(req.params.userId);
   res.send(user);
 }));
 
-router.patch('/:userId', validate(UserUpdateDto), userMiddleware, handler(async (req, res) => {
+router.patch('/users/:userId', validate(UserUpdateDto), userMiddleware, handler(async (req, res) => {
   const user = await controllers.updateUser(req.params.userId, req.body);
   res.send(user);
 }));
 
-router.delete('/:userId', userMiddleware, handler(async (req, res) => {
+router.delete('/users/:userId', userMiddleware, handler(async (req, res) => {
   await controllers.deleteUser(req.params.userId);
   res.sendStatus(httpStatus.NO_CONTENT);
 }));
