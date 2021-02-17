2a2518382399acc4693a3c645b2424745b748243
LeChatErrant <guillaume.hector@epitech.eu>
2021-02-17 04:22:39 +0100

    refacto: new route formatting (cleaner format, needed with longer routes)

--- src/components/user/userRoutes.ts
+++ src/components/user/userRoutes.ts
@@ -11,38 +11,63 @@ import { UserSignupDto, UserSigninDto, UserUpdateDto } from './userTypes';
 
 const router = express.Router();
 
-router.get('/users', adminMiddleware, handler(async (req, res) => {
-  const users = await controllers.listUsers();
-  res.send(users);
-}));
+router.get(
+  '/users',
+  adminMiddleware,
+  handler(async (req, res) => {
+    const users = await controllers.listUsers();
+    res.send(users);
+  }),
+);
 
-router.post('/users/signup', validate(UserSignupDto), handler(async (req, res) => {
-  const user = await controllers.signup(req.body);
-  res.status(httpStatus.CREATED).send(user);
-}));
+router.post(
+  '/users/signup',
+  validate(UserSignupDto),
+  handler(async (req, res) => {
+    const user = await controllers.signup(req.body);
+    res.status(httpStatus.CREATED).send(user);
+  }),
+);
 
-router.post('/users/signin', validate(UserSigninDto), handler(async (req, res) => {
-  const user = await controllers.signin(req.body);
-  req.session.user = {
-    id: user.id,
-    role: user.role,
-  };
-  res.send(user);
-}));
+router.post(
+  '/users/signin',
+  validate(UserSigninDto),
+  handler(async (req, res) => {
+    const user = await controllers.signin(req.body);
+    req.session.user = {
+      id: user.id,
+      role: user.role,
+    };
+    res.send(user);
+  }),
+);
 
-router.get('/users/:userId', userMiddleware, handler(async (req, res) => {
-  const user = await controllers.getUser(req.params.userId);
-  res.send(user);
-}));
+router.get(
+  '/users/:userId',
+  userMiddleware,
+  handler(async (req, res) => {
+    const user = await controllers.getUser(req.params.userId);
+    res.send(user);
+  }),
+);
 
-router.patch('/users/:userId', validate(UserUpdateDto), userMiddleware, handler(async (req, res) => {
-  const user = await controllers.updateUser(req.params.userId, req.body);
-  res.send(user);
-}));
+router.patch(
+  '/users/:userId',
+  validate(UserUpdateDto),
+  userMiddleware,
+  handler(async (req, res) => {
+    const user = await controllers.updateUser(req.params.userId, req.body);
+    res.send(user);
+  }),
+);
 
-router.delete('/users/:userId', userMiddleware, handler(async (req, res) => {
-  await controllers.deleteUser(req.params.userId);
-  res.sendStatus(httpStatus.NO_CONTENT);
-}));
+router.delete(
+  '/users/:userId',
+  userMiddleware,
+  handler(async (req, res) => {
+    await controllers.deleteUser(req.params.userId);
+    res.sendStatus(httpStatus.NO_CONTENT);
+  }),
+);
 
 export default router;
