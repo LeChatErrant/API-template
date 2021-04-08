5a75356118730e92e866223951fee64908f56bb7
LeChatErrant <guillaume.hector@epitech.eu>
2021-04-08 21:32:02 +0200

    fix: gracefully terminate prisma query engine after main process

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import app from './app';
-import { config } from './appConfig';
+import db from './appDatabase';
 import logger from './appLogger';
+import { config } from './appConfig';
 import waitApp from './utils/waitApp';
 import seedAdminUser from './utils/seedAdminUser';
 
 const { port } = config;
 
-(async function main() {
+async function main() {
   await waitApp();
   await seedAdminUser();
   app.listen(port, () => logger.info(`Server listening on port ${port} on mode ${config.mode}...`));
-}());
+}
+
+main()
+  .catch((error) => {
+    logger.error(error);
+    throw error;
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
