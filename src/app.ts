0442bf75c55fa5df1548844f2fc9c3c414427519
LeChatErrant <guillaume.hector@epitech.eu>
2021-12-15 21:32:13 +0100

    fix: correctly using helmet

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(session);
 /*  Middlewares */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet);
+app.use(helmet());
 app.use(meMiddleware);
 app.use(requestLogger);
 
