a5188ca6cbd3519a55fd3c7dfbf05bd0dec25ff1
LeChatErrant <guillaume.hector@epitech.eu>
2022-05-09 12:42:56 +0200

    feat: utility to add months to date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -23,3 +23,16 @@ export function addDaysToDate(date: Date, days: number) {
   newDate.setDate(date.getDate() + days);
   return newDate;
 }
+
+/**
+ * Add months to a given date
+ *
+ * @param date Date to increase
+ * @param months Number of months
+ * @return New date
+ */
+export function addMonthsToDate(date: Date, months: number) {
+  const newDate = new Date(date);
+  newDate.setMonth(date.getMonth() + months);
+  return newDate;
+}
