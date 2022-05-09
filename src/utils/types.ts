7dfc5c0fbd3b32380758386bad30f27adc916328
LeChatErrant <guillaume.hector@epitech.eu>
2022-05-09 12:39:03 +0200

    feat: documented utility types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,11 @@
+/**
+ * Either a type or a nullish value
+ */
 export type Maybe<T> = T | null | undefined;
+
+/**
+ * Type definition that will be written later
+ *
+ * Useful when mapping external API objects to indicate a key that have not been already typed
+ */
+export type Todo = unknown;
