067f988311250a4775c3ec80187c46cac39d8713
LeChatErrant <guillaume.hector@epitech.eu>
2022-05-17 11:14:32 +0200

    refacto: using suggested import names for routes

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,14 +1,14 @@
 import { AsyncRouter } from 'express-async-router';
 
-import posts from '@routes/post/post.routes';
-import users from '@routes/user/user.routes';
+import postRoutes from '@routes/post/post.routes';
+import userRoutes from '@routes/user/user.routes';
 
 const router = AsyncRouter();
 
 router.get('/', (req, res) => res.send('Welcome to the backend world !'));
 router.get('/ping', (req, res) => res.send('pong'));
 
-router.use(users);
-router.use(posts);
+router.use(userRoutes);
+router.use(postRoutes);
 
 export default router;
