877282bf481c5903e0f8b565eb0b2c7dfdb412ab
LeChatErrant <guillaume.hector@epitech.eu>
2022-08-30 17:18:06 +0200

    feat: misc utils added

---
+++ src/utils/array.ts
@@ -0,0 +1,36 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+/**
+ * Group by regroup elements of an array depending on the value of the given field
+ *
+ * @param arr The array to group
+ * @param by Name of the key to group by
+ *
+ * @example
+ * const inventory = [
+ *   { name: 'asparagus', type: 'vegetable', quantity: 5 },
+ *   { name: 'banana',  type: 'fruit', quantity: 0 },
+ *   { name: 'cow', type: 'meat', quantity: 23 },
+ *   { name: 'cherry', type: 'fruit', quantity: 5 },
+ *   { name: 'fish', type: 'viande', quantity: 22 },
+ * ];
+ *
+ * groupBy(inventory, 'type');
+ * // {
+ * //   vegetable: [ { name: 'asparagus', type: 'vegetable', quantity: 5 } ],
+ * //   fruit: [
+ * //     { name: 'banana', type: 'fruit', quantity: 0 },
+ * //     { name: 'cherry', type: 'fruit', quantity: 5 }
+ * //   ],
+ * //   meat: [ { name: 'cow', type: 'meat', quantity: 23 } ],
+ * //   viande: [ { name: 'fish', type: 'viande', quantity: 22 } ]
+ * // }
+ */
+export function groupBy<T extends Record<string, any>, K extends keyof T>(arr: Array<T>, by: K): Record<T[K], Array<T>> {
+  return arr.reduce((acc, val) => ({
+    ...acc,
+    [val[by]]: acc[val[by]]
+      ? [...acc[val[by]], val]
+      : [val],
+  }), {} as Record<T[K], Array<T>>);
+}
--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -4,6 +4,16 @@
  * @param str The string to capitalize
  * @return The capitalized string
  */
-export function capitalize(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+export function capitalize<T extends string>(str: T): Capitalize<T> {
+  return str.charAt(0).toUpperCase() + str.slice(1) as Capitalize<T>;
+}
+
+/**
+ * Uncapitalize the given string
+ *
+ * @param str The string to uncapitalize
+ * @return The uncapitalized string
+ */
+export function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
+  return str.charAt(0).toLowerCase() + str.slice(1) as Uncapitalize<T>;
 }
