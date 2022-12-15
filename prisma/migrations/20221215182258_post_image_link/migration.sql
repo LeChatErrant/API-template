bd9f4663387d9bff34f9b99e3db779a976b579ba
LeChatErrant <guillaume.hector@epitech.eu>
2022-12-15 19:23:11 +0100

    db: image link migration

---
+++ prisma/migrations/20221215182258_post_image_link/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Made the column `username` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "imageLink" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "username" SET NOT NULL;
